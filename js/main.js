c4f9ef413465efc4cbaaab9bf31734b093b159d2
gabypizzutti <gabrielapizzutti@gmail.com>
2022-07-22 18:49:14 -0300

    se hace animacion de receta de pizza y noquis

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 
+// // para mostrar la receta de la pizza
+// function mostrarReceta(){
+//     document.getElementById('receta1').style.display = 'block';
+//     document.getElementById('recetaD1').style.display = 'block';
+// }
+
+
+// let btnOcultar = document.getElementById("receta1");
+// btnOcultar.addEventListener("click", ocultarBoton);
+
+// // funcion para ocultar la receta y ver otra receta
+// function ocultarReceta(){
+//     document.getElementById('receta1').style.display = "none";
+//     document.getElementById('recetaD1').style.display = "none";
+// }
+
+
+
+ 
 // para mostrar la receta de la pizza
 function mostrarReceta(){
     document.getElementById('receta1').style.display = 'block';
-    document.getElementById('recetaD1').style.display = 'block';
-}
-
-
-let btnOcultar = document.getElementById("receta1");
-btnOcultar.addEventListener("click", ocultarBoton);
-
-// funcion para ocultar la receta y ver otra receta
-function ocultarReceta(){
+    // document.getElementById('receta2').style.display = 'block';
+ }
+  
+  
+ let btnOcultar = document.getElementById("receta1");
+ btnOcultar.addEventListener("click", ocultarBoton);
+  
+ // funcion para ocultar la receta y ver otra receta
+ function ocultarReceta(){
     document.getElementById('receta1').style.display = "none";
-    document.getElementById('recetaD1').style.display = "none";
-}
+    // document.getElementById('receta2').style.display = "none";
+
+ }
+ 
