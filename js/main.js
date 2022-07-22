550d0e4206d1479afadfcffbc3c154c68dcbb3d2
euge-gomez <94846681+euge-gomez@users.noreply.github.com>
2022-07-22 00:38:40 -0300

    Sorteo y Recetas Dulces

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 // para mostrar la receta de la pizza
 function mostrarReceta(){
     document.getElementById('receta1').style.display = 'block';
+    document.getElementById('recetaD1').style.display = 'block';
 }
 
 
@@ -11,4 +12,5 @@ btnOcultar.addEventListener("click", ocultarBoton);
 // funcion para ocultar la receta y ver otra receta
 function ocultarReceta(){
     document.getElementById('receta1').style.display = "none";
+    document.getElementById('recetaD1').style.display = "none";
 }
